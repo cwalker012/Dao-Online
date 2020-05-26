8fec07e14282acaa8faad1253b6a5d981291bf24
Cameron <44031235+cwalker012@users.noreply.github.com>
2020-05-25 20:06:21 -0500

    Pieces can be selected and highlighted

--- src/game.js
+++ src/game.js
@@ -3,6 +3,10 @@ var config = {
     width: 800,
     height: 800,
     backgroundColor: '#d4bd8a',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     scene: {
         preload: preload,
         create: create
@@ -10,28 +14,57 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('black', 'assets/black.png');
-    this.load.image('white', 'assets/white.png');
-}
-
-function create ()
-{
-    var board =[["white", "", "", "black"],
+var selectedPiece = null;
+var pieces;
+var board =[["white", "", "", "black"],
             ["", "white","black", ""],
             ["", "black", "white", ""],
             ["black", "", "", "white"]];
 
+
+function preload ()
+{
+    this.load.image('black', './src/assets/black.png');
+    this.load.image('white', './src/assets/white.png');
+}
+
+function create ()
+{
+    pieces = this.add.group();
     for(i = 0; i < board.length; i++){
         for(j = 0; j < board[i].length; j++){
             if (board[i][j] === "white"){
-                var sprite = this.add.sprite(i*200+100, j*200+100, 'white');
+                var piece = pieces.create(i*200+100, j*200+100, 'white');
+                piece.name = 'piece' + i.toString() + 'x' + j.toString();
+                piece.setInteractive();
+                //setPos(piece, i, j);
+                piece.on('clicked', select, this);
             }
             else if(board[i][j] === "black"){
-                var sprite = this.add.sprite(i*200+100, j*200+100, 'black');
+                var piece = pieces.create(i*200+100, j*200+100, 'black');
+                piece.name = 'piece' + i.toString() + 'x' + j.toString();
+                piece.setInteractive();
+                //setPos(piece, i, j);
+                piece.on('clicked', select, this);
             }
+            
         }
     }
+    this.input.on('gameobjectup', function (pointer, gameObject)
+    {
+        gameObject.emit('clicked', gameObject);
+    }, this);
+}
+
+function select(piece){
+    if (piece == selectedPiece){
+        piece.clearTint();
+        selectedPiece = null;
+    }
+    else{
+        selectedPiece = piece;
+        piece.setTintFill(0xff0000);
+    }
+    console.log(piece);
+    console.log('wow');
 }
